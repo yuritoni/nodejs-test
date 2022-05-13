971c8a777ec25612ce49e7329aced0f291ff37b5
Yusri Fathoni <yusrifathoni@yahoo.co.id>
2022-05-13 21:51:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -46,9 +46,13 @@ app.get('/', (req, res) => {
 app.use('/api', require('./routes/api'))
 
 // open the server
-app.listen(port, () => {
-    console.log(`Express is running on port ${port}`)
-})
+//app.listen(port, () => {
+//    console.log(`Express is running on port ${port}`)
+//})
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 
 
